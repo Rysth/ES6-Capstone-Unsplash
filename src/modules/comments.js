f8fffb8849560ebd4a035ac599b3deff18f81d50
Rysth <johnpalacios.t@gmail.com>
2023-08-02 10:53:28 -0500

    Update modal card dynamically based on a ID.

--- src/modules/comments.js
+++ src/modules/comments.js
@@ -1,10 +1,33 @@
 import { getImageByID } from './unsplash';
 
 window.addEventListener('load', async () => {
-  const modalImageElement = document.querySelector('#modal-image');
-  const imageData = await getImageByID(2);
+  const modalElement = document.querySelector('#modal');
+  const modalInformation = document.querySelector('#modal-information');
+  const imageData = await getImageByID(4);
 
   if (imageData) {
-    modalImageElement.src = imageData.download_url;
+    modalInformation.innerHTML = `
+       <img
+          loading="lazy"
+          id="modal-image"
+          src="${imageData.download_url}"
+          alt="Preview Image - ${imageData.author}"
+          class="modal-image"
+        />
+        <p class="modal-title">${imageData.author}</p>
+        <ul class="modal-list">
+          <li class="modal-item"><span class="fw-bold">Width:</span> ${imageData.width}px</li>
+          <li class="modal-item">
+            <span class="fw-bold">Height:</span> ${imageData.height}px
+          </li>
+          <li class="modal-item"><span class="fw-bold">Author:</span> ${imageData.author}</li>
+          <li class="modal-item">
+            <span class="fw-bold">Page:</span> 
+            <a href="${imageData.url}" id="page">Credits</a>
+          </li>
+        </ul>
+    `;
+
+    modalElement.style.display = 'block';
   }
 });
