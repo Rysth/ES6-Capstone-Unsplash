19b7f685aefc4eac9f1df52db0a9d1e1ff3f0c00
Rysth <johnpalacios.t@gmail.com>
2023-08-03 09:42:10 -0500

    Update comments.css and unsplash.js

--- src/modules/unsplash.js
+++ src/modules/unsplash.js
@@ -1,4 +1,16 @@
-const API = 'https://picsum.photos/';
+const API = 'https://picsum.photos';
+
+const getImages = async () => {
+  const response = await fetch(`${API}/v2/list?page=2&limit=6`, {
+    method: 'GET',
+  });
+
+  const data = await response.json();
+
+  if (data) return data;
+
+  return 'Nothing found';
+};
 
 const getImageByID = async (imageID) => {
   const response = await fetch(`${API}/seed/${imageID}/info`, {
