fdb81acc75f604528f1ce324532a6f389efe38e4
Rysth <johnpalacios.t@gmail.com>
2023-08-03 17:14:13 -0500

    Update dist folder and fix linter errors.

--- src/modules/counters/comments.js
+++ src/modules/counters/comments.js
@@ -2,10 +2,7 @@ const countComments = (information) => {
   if (information) {
     const textComment = document.querySelector('#text-comment');
     textComment.innerHTML = `Comments(${information.length})`;
-    return;
   }
-
-  return 0;
 };
 
 module.exports = {
